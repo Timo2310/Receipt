3e452e5266f86789e5eaf5f7786a4d7542f37397
Timo2310 <105223611+Timo2310@users.noreply.github.com>
2022-05-10 15:53:53 +0200

    added basic Item structure

--- shopping/Item.java
+++ shopping/Item.java
@@ -1,5 +1,47 @@
 package shopping;
 
 public class Item {
+	
+	private String name;
+	private boolean type;
+	private boolean imported;
+	private double baseprice;
+	private double taxprice;
+	
+	
+	public Item (String productinfo, boolean type) {
+		processPrInfo();
+		this.type = type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public boolean getType() {
+		return type;
+	}
+	
+	public double getPrice() {
+		return baseprice;
+	}
+	
+	public boolean isImported() {
+		return imported;
+	}
+	
+	public void processPrInfo() {
+		
+	}
+	
+	public double getTaxPrice() {
+		return taxprice;
+	}
+	
+	public double getTax() {
+		return taxprice-baseprice;
+	}
+	
 
 }
+
