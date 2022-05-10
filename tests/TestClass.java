87bee92d3d776587cb862e22ce00e427dcf89bf1
Timo2310 <105223611+Timo2310@users.noreply.github.com>
2022-05-10 19:51:08 +0200

    Added more tests
    - added the final form of the tests

--- tests/TestClass.java
+++ tests/TestClass.java
@@ -6,9 +6,12 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
 import shopping.Item;
+import shopping.Receipt;
 
 
 public class TestClass {
+	
+	Receipt receipt = new Receipt();
 	
 	Item item1 = new Item("book at 12.49", true);
 	Item item2 = new Item("music CD at 14.99", false);
@@ -40,14 +43,34 @@ public class TestClass {
 		assertEquals(54.65, item5.getTaxPrice(), 0.009);
 		assertEquals(7.15, item5.getTax(), 0.009);
 		assertEquals(20.89, item6.getTaxPrice(), 0.009);
-		assertEquals(2.10, item6.getTax(), 0.009);
-		
+		assertEquals(1.90, item6.getTax(), 0.009);
 	}
 	
+	@Test
+	@DisplayName("Total Price3")
+	public void calcTotal3() {
+		receipt.add("1 imported bottle of perfume at 27.99", false);
+		receipt.add("1 bottle of perfume at 18.99", false);
+		receipt.add("1 packet of headache pills at 9.75", true);
+		receipt.add("1 box of imported chocolates at 11.25", true);
+		assertEquals(6.70, receipt.getSalesTaxTotal(), 0.009);
+		assertEquals(74.68, receipt.getTotal(), 0.009);
+		assertEquals(true, receipt.printReceipt());
+	}
 	
 	@Test
-	@DisplayName("Total Price")
-	public void calcTotal() {
+	@DisplayName("Total Price2")
+	public void calcTotal2() {
+		receipt.add("1 imported box of chocolates at 10.00", true);
+		receipt.add("1 imported bottle of perfume at 47.50", false);
+		assertEquals(7.65, receipt.getSalesTaxTotal(), 0.009);
+		assertEquals(65.15, receipt.getTotal(), 0.009);
+		assertEquals(true, receipt.printReceipt());
+	}
+	
+	@Test
+	@DisplayName("Total Price1")
+	public void calcTotal1() {
 		receipt.add("1 book at 12.49", true);
 		receipt.add("1 music CD at 14.99", false);
 		receipt.add("1 chocolate bar at 0.85", true);
@@ -57,3 +80,4 @@ public class TestClass {
 	}
 
 }
+
